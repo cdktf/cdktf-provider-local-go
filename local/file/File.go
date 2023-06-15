b72c92ee7c5c0092d0fb125e1e9e00c1d97180b8
CDK for Terraform Team <github-team-tf-cdk@hashicorp.com>
2023-06-15 11:27:11 +0000

    chore(release): v7.0.0

--- local/file/File.go
+++ local/file/File.go
@@ -2,10 +2,10 @@ package file
 
 import (
 	_jsii_ "github.com/aws/jsii-runtime-go/runtime"
-	_init_ "github.com/cdktf/cdktf-provider-local-go/local/v6/jsii"
+	_init_ "github.com/cdktf/cdktf-provider-local-go/local/v7/jsii"
 
 	"github.com/aws/constructs-go/constructs/v10"
-	"github.com/cdktf/cdktf-provider-local-go/local/v6/file/internal"
+	"github.com/cdktf/cdktf-provider-local-go/local/v7/file/internal"
 	"github.com/hashicorp/terraform-cdk-go/cdktf"
 )
 
--- local/sensitivefile/SensitiveFile.go
+++ local/sensitivefile/SensitiveFile.go
@@ -2,10 +2,10 @@ package sensitivefile
 
 import (
 	_jsii_ "github.com/aws/jsii-runtime-go/runtime"
-	_init_ "github.com/cdktf/cdktf-provider-local-go/local/v6/jsii"
+	_init_ "github.com/cdktf/cdktf-provider-local-go/local/v7/jsii"
 
 	"github.com/aws/constructs-go/constructs/v10"
-	"github.com/cdktf/cdktf-provider-local-go/local/v6/sensitivefile/internal"
+	"github.com/cdktf/cdktf-provider-local-go/local/v7/sensitivefile/internal"
 	"github.com/hashicorp/terraform-cdk-go/cdktf"
 )
 
